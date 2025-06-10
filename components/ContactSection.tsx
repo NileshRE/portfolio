5bc241b96433c4ea7065ac79179c9e681d0d9c54
Nilesh Kumar <19nilesh.kumar@gmail.com>
2025-06-10 13:28:24 +0530

    FIX:Link opening in same page issue

--- components/ContactSection.tsx
+++ components/ContactSection.tsx
@@ -18,13 +18,17 @@ const ContactSection = () => {
           </p>
           <div className="flex flex-col sm:flex-row gap-4 justify-center">
             <Button size="lg" asChild>
-              <Link href={links.mail}>
+              <Link href={links.mail} target="_blank" rel="noopener noreferrer">
                 <Mail className="mr-2 h-4 w-4" />
                 Send Email
               </Link>
             </Button>
             <Button size="lg" variant="outline" asChild>
-              <Link href={links.linkedin}>
+              <Link
+                href={links.linkedin}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <Linkedin className="mr-2 h-4 w-4" />
                 LinkedIn
               </Link>
