685dea7f4e94326b18ce58495d012cbebb44610b
Nilesh Kumar <19nilesh.kumar@gmail.com>
2025-09-28 18:48:38 +0530

    ADD:OG images

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
-import "./globals.css";
 import { Fira_Code } from "next/font/google";
+import "./globals.css";
 
 const firaCode = Fira_Code({
   subsets: ["latin"],
@@ -12,6 +12,12 @@ export const metadata: Metadata = {
     "Nilesh Kumar | Frontend Developer Portfolio – React, JavaScript, UI Engineer",
   description:
     "Explore the professional portfolio of Nilesh Kumar, a skilled frontend developer specializing in React, JavaScript, and modern UI/UX. View projects, experience, and tech stack expertise.",
+  openGraph: {
+    images: ["/images/opengraph-image.png"],
+  },
+  twitter: {
+    images: ["/images/opengraph-image.png"],
+  },
 };
 
 export default function RootLayout({
